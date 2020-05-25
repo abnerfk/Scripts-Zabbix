9ab3f59ed1bafd16b46ce6b468f63746c0139d7c
Abner Klug <42499278+abnerfk@users.noreply.github.com>
2020-05-25 11:47:00 -0300

    Create zabbix-cpumemorialinux.sh

---
+++ zabbix-cpumemorialinux.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+mkdir /home/scripts
+chown zabbix:zabbix /home/scripts
+chmod 744 /home/scripts
+cd /home/scripts
+wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/discovertop5cpu.sh
+wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/discovertop5memory.sh
+wget https://github.com/abnerfk/Scripts-Zabbix/blob/master/sessions.sh
+chmod 744 *
+chown zabbix:zabbix *
+if [ 'grep -w "# Timeout=3" /etc/zabbix/zabbix_agentd.conf' ]; then
+  sed -i 's/# Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agentd.conf
+elif [ 'grep -w "Timeout=3" /etc/zabbix/zabbix_agentd.conf']; then
+  sed -i 's/Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agentd.conf
+fi
+  echo "UserParameter=discovery.processosmemoria.linux[*],/bin/bash /home/scripts/discovertop5memory.sh $1 $2" >> /etc/zabbix/zabbix_agentd.conf
+  echo "UserParameter=discovery.processoscpu.linux[*],/bin/bash /home/scripts/discovertop5cpu.sh $1" >> /etc/zabbix/zabbix_agentd.conf
+  systemctl restart zabbix-agent;
