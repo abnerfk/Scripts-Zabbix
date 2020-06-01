194eab95fbec31354635bf67f28987e554c7de7d
Abner Klug <42499278+abnerfk@users.noreply.github.com>
2020-06-01 16:44:20 -0300

    Update zabbix-cpumemorialinux.sh

--- zabbix-cpumemorialinux.sh
+++ zabbix-cpumemorialinux.sh
@@ -13,8 +13,8 @@ if [ '$(grep -w "# Timeout=3" /etc/zabbix/zabbix_agentd.conf | wc -l) -eq 1' ]; 
 elif [ '$(grep -w "Timeout=3" /etc/zabbix/zabbix_agentd.conf | wc -l) -eq 1' ]; then
   sed -i 's/Timeout=3/Timeout=30/g' /etc/zabbix/zabbix_agentd.conf
 fi
-  echo "UserParameter=discovery.processosmemoria.linux[*],/bin/bash /home/scripts/discovertop5memory.sh $1 $2" >> /etc/zabbix/zabbix_agentd.conf
-  echo "UserParameter=discovery.processoscpu.linux[*],/bin/bash /home/scripts/discovertop5cpu.sh $1" >> /etc/zabbix/zabbix_agentd.conf
+  echo "UserParameter=discovery.processosmemoria.linux[*],/bin/bash /home/scripts/discovertop5memory.sh \$1 \$2" >> /etc/zabbix/zabbix_agentd.conf
+  echo "UserParameter=discovery.processoscpu.linux[*],/bin/bash /home/scripts/discovertop5cpu.sh \$1" >> /etc/zabbix/zabbix_agentd.conf
   echo "UserParameter=sessions.active,/bin/bash /home/scripts/sessions.sh" >> /etc/zabbix/zabbix_agentd.conf
   echo "zabbix	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
   service zabbix-agent restart
