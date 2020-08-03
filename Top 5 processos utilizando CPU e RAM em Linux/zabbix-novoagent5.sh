bec9410d0214bbd82a5e4fffbc9b7d347fa3e8b0
Abner Klug <abnerfklug@gmail.com>
2020-08-03 14:29:10 -0300

    Update zabbix-novoagent5.sh

--- Top 5 processos utilizando CPU e RAM em Linux/zabbix-novoagent5.sh
+++ Top 5 processos utilizando CPU e RAM em Linux/zabbix-novoagent5.sh
@@ -13,7 +13,6 @@ wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/Top%205%20p
 wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/Top%205%20processos%20utilizando%20CPU%20e%20RAM%20em%20Linux/zabbix_agentd.d/monitoring_cpu_memory_process.conf
 echo Insira o hostname do agent:
 read hostname
-  Hostname=Zabbix server
   sed -i 's/Hostname=Zabbix server/Hostname=$hostname/g' /etc/zabbix/zabbix_agentd.conf
   sed -i 's/Server=127.0.0.1/Server=zabbix.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
   sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
