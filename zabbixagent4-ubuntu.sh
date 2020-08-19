59eeebd4ad909bb53067529aedcac402c8f4c737
Abner Klug <abnerfklug@gmail.com>
2020-08-19 16:26:58 -0300

    Update zabbixagent4-ubuntu.sh

--- zabbixagent4-ubuntu.sh
+++ zabbixagent4-ubuntu.sh
@@ -4,21 +4,13 @@ wget https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix
 dpkg -i https://repo.zabbix.com/zabbix/4.0/ubuntu/pool/main/z/zabbix-release/zabbix-release_4.0-3+xenial_all.deb
 apt update
 apt install -y zabbix-agent
-mkdir /home/scripts
-chown zabbix:zabbix /home/scripts
-chmod 744 /home/scripts
-cd /home/scripts
-wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/Top%205%20processos%20utilizando%20CPU%20e%20RAM%20em%20Linux/Scripts/sessions.sh
-chmod 744 *
-chown zabbix:zabbix *
 cd /etc/zabbix/zabbix_agentd.d
 wget https://raw.githubusercontent.com/abnerfk/Scripts-Zabbix/master/conf.d/2cloud-4.conf
 echo Insira o hostname do agent:
 read hostname
   sed -i "s/Hostname=Zabbix server/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/Server=127.0.0.1/Server=zabbix.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
-  sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
-  echo "zabbix	ALL=(ALL)	NOPASSWD: ALL" >> /etc/sudoers
+  sed -i 's/Server=127.0.0.1/Server=zabbix.2cloud.com.br,zabbixnovo.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
+  sed -i 's/ServerActive=127.0.0.1/ServerActive=zabbix.2cloud.com.br,zabbixnovo.2cloud.com.br/g' /etc/zabbix/zabbix_agentd.conf
   service zabbix-agent restart
   systemctl enable zabbix-agent
   cat /etc/zabbix/zabbix_agentd.conf | grep Server=
